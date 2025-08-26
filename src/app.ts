95b3cf339b310f9e809341522cd504603bca8bd9
AshanOdi <ashanuni25@gmail.com>
2025-08-26 08:40:22 +0530

    implemented routes

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express,{Application} from "express";
+import greetingRouter from "./routes/greeting.route";
 
 
 const app:Application = express();
 
 app.use(express.json());
-
-
+app.use("/greeting",greetingRouter);
 app.get("/", (req, res) => {
     res.send("Hello World");
 });
