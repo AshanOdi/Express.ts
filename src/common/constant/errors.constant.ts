e2a013c9b35aa587a3c1c25ca7843b5b7e293980
AshanOdi <ashanuni25@gmail.com>
2025-08-26 17:37:32 +0530

    Update package dependencies and refactor routing structure; remove dist files and add new error handling interfaces

---
+++ src/common/constant/errors.constant.ts
@@ -0,0 +1,16 @@
+export const Error = {
+    BAD_REQUEST : {
+        key: 'BAD_REQUEST';
+        message: 'Invalid Syntax or missing Parameters';
+    },
+
+    UNAUTHORIZED : {
+        key: 'UNAUTHORIZED';
+        message:'you are not allowed to access this resource'
+    },
+
+    LOGIN_FAILED:{
+        key: 'LOGIN_FAILED';
+        message:'Invalid username or password';
+    }
+}
